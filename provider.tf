2c0cdf1f830a76f23038c1831b588cd191204fcc
sree4071 <sree4071@gmail.com>
2025-08-26 12:18:25 +0530

    updated provider.tf

--- provider.tf
+++ provider.tf
@@ -10,8 +10,5 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = "8f14c5c2-3c21-4b37-9908-78f919229a17"
-  client_id = "2245129a-d5bc-4d9f-8d80-fcbfb791976c"
-  tenant_id = "def79301-d70d-4dcf-8f56-bc3526aa06c1"
-
+  
 }
