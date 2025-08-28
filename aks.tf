71a82305adc003343e0054baf8e54a3a5f7ad042
sree4071 <sree4071@gmail.com>
2025-08-28 23:05:25 +0530

    updated the k8 version

--- aks.tf
+++ aks.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name                = "system"
     node_count          = var.system_node_count
-    vm_size             = "Standard_DS2_v2"
+    vm_size             = "standard_a2_v2"
     type                = "VirtualMachineScaleSets"
     #zones  = [1, 2, 3]
     #enable_auto_scaling = false
